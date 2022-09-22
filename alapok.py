434d535d9e8ad2e4450ade054e9f9bbc64e21974
andrasinorbert92 <113181319+andrasinorbert@users.noreply.github.com>
2022-09-22 10:21:26 +0200

    órai kód

--- alapok.py
+++ alapok.py
@@ -1,2 +1,29 @@
 print("hello")
 
+t=[3,5,8,10,3,6]
+
+# print(sum(t))
+
+# Összegzés (Sorozatszámítás) tétele
+sum=0
+for i in t:
+    sum+=i
+
+print(sum)
+
+sum=0
+i=0
+while i<len(t):
+    sum+=t[i]
+    i+=1
+
+print(sum)
+
+print("átlag:", sum/len(t))
+print("kerekítve:", round(sum/len(t)))
+print("lefele kerekítve:", int(sum/len(t)))
+
+num= 6.4
+
+print("felfele kerekítés:", int(num)+1)
+
