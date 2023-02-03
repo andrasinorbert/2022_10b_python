e2080b8d1ced6cf65a525e7ca8e5e2f0a191f81b
andrasinorbert92 <113181319+andrasinorbert@users.noreply.github.com>
2023-02-03 12:37:37 +0100

    20230202_korrep

---
+++ Korrep/20230202/bubblesort.py
@@ -0,0 +1,5 @@
+def bubbleSort(lista):
+    for i in range(len(lista)):
+        for j in range(0, len(lista)-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], arr[j]
